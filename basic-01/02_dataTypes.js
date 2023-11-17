f3116e9106a9094ba5dc8d3ba9c808ae5cc8c8ca
786Mushfique <114329904+786Mushfique@users.noreply.github.com>
2023-11-17 06:28:00 +0000

    dataTypes

---
+++ basic-01/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert("3+3"); // we are using node js not browser
+
+
+
+let name = "Raza";
+let age= 18;
+
+let loggedIn = false;
+
+// number => 2 to the power 53
+// bigint
+// string = ""
+// boolean = true or false
+// null
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
