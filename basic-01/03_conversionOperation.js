6479b45e148b235a62989f02f8cfbcd905520666
786Mushfique <114329904+786Mushfique@users.noreply.github.com>
2023-11-18 04:42:49 +0000

    conversionAndOperation

--- basic-01/03_conversionOperation.js
+++ basic-01/03_conversionOperation.js
@@ -20,5 +20,31 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//******************************operation***********************/
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+let str1 = "hello";
+let str2 = "Hitesh";
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log("2" + 1);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "1");
+
+// console.log((3 + 4) * 5 % 3);
+
+
+
+let num1 , num2 , num3 ;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
