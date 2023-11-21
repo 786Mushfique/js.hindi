3d11e4010c47df37b314e0577e79502d217f5eeb
786Mushfique <114329904+786Mushfique@users.noreply.github.com>
2023-11-21 13:09:44 +0000

    functions

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -34,4 +34,36 @@ function loginUserMessage(username){
 }
 // console.log(loginUserMessage("hitesh"))
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(...num1){ //rest operator
+    return num1;
+
+}
+// console.log(calculateCartPrice(200,300,444))
+
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
