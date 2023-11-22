c58d62f98120a7d6e1235db44ddf3cdf3549a52d
786Mushfique <114329904+786Mushfique@users.noreply.github.com>
2023-11-22 13:28:36 +0000

    iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+//while loop
+// let index = 0;
+// while (index<=10) {
+
+//     console.log(`value of index ${index}`)
+//     index = index + 2;
+
+
+    
+// }
+
+// let myArray = ["flash" , "batman" , "superman"];
+
+// let i =0;
+// while(i < myArray.length){
+//     console.log(`value is ${myArray[i]}`)
+
+//     i++;
+// }
+
+// do while loop
+
+let score =1;
+do {
+    console.log(`score is ${score}`)
+    score++;
+    
+} while (score<=10);
