020d3be49f49c6dd45bece7bca0fed4e55b4a96e
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2024-01-31 16:52:36 +0530

    Update 05_string.js

--- basic-01/05_string.js
+++ basic-01/05_string.js
@@ -5,6 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
+// declearation of string
 const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
@@ -16,7 +17,7 @@ const gameName = new String('hitesh-hc-com')
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-// declearation of string 
+
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
